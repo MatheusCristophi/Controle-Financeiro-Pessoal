5f5ca9d192559f695079e8c05e55e1af61e61b64
MatheusCristophi <matheuscr909@gmail.com>
2026-06-16 23:41:28 -0300

    Adicionando pastas iniciais

---
+++ backend/src/main/java/com/Matheus/GestaoFinanceira/User/entity/User.java
@@ -0,0 +1,25 @@
+package com.Matheus.GestaoFinanceira.User.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    private UUID id;
+    
+
+}
