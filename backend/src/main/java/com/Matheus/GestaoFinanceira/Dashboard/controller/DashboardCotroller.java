75fbd05a5062005048777f6d8feaebcde723e67f
MatheusCristophi <matheuscr909@gmail.com>
2026-06-22 05:43:15 -0300

    dashboard inicial completo

---
+++ backend/src/main/java/com/Matheus/GestaoFinanceira/Dashboard/controller/DashboardCotroller.java
@@ -0,0 +1,53 @@
+package com.Matheus.GestaoFinanceira.Dashboard.controller;
+
+import com.Matheus.GestaoFinanceira.Dashboard.service.DashboardService;
+import com.Matheus.GestaoFinanceira.User.entity.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("dashboard")
+public class DashboardCotroller {
+
+    private final DashboardService service;
+
+    public DashboardCotroller(DashboardService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/balance")
+    public ResponseEntity<BigDecimal> getCurrentBalance(@AuthenticationPrincipal User user){
+        BigDecimal balance = service.currentBalance(user);
+        return new ResponseEntity<>(balance,HttpStatus.OK);
+    }
+
+    @GetMapping("/expenses")
+    public ResponseEntity<BigDecimal> getAllExpenses(@AuthenticationPrincipal User user){
+        BigDecimal balanceExpenses = service.balanceExpenseForMonth(user);
+        return new ResponseEntity<>(balanceExpenses, HttpStatus.OK);
+    }
+
+    @GetMapping("/incomes")
+    public ResponseEntity<BigDecimal> getAllIncomes(@AuthenticationPrincipal User user){
+        BigDecimal balanceIncomes = service.balanceIncomeForMonth(user);
+        return new ResponseEntity<>(balanceIncomes, HttpStatus.OK);
+    }
+
+    @GetMapping("expenses/month")
+    public ResponseEntity<BigDecimal> getAllExpensesOfPreviousMonth(@AuthenticationPrincipal User user){
+        BigDecimal balanceExpenses = service.balanceExpenseForPreviousMonth(user);
+        return new ResponseEntity<>(balanceExpenses, HttpStatus.OK);
+    }
+
+    @GetMapping("incomes/month")
+    public ResponseEntity<BigDecimal> getAllIncomesOfPreviousMonth(@AuthenticationPrincipal User user){
+        BigDecimal balanceIncomes = service.balanceIncomeForPreviousMonth(user);
+        return new ResponseEntity<>(balanceIncomes, HttpStatus.OK);
+    }
+}
--- backend/src/main/java/com/Matheus/GestaoFinanceira/Dashboard/service/DashboardService.java
+++ backend/src/main/java/com/Matheus/GestaoFinanceira/Dashboard/service/DashboardService.java
@@ -2,14 +2,13 @@ package com.Matheus.GestaoFinanceira.Dashboard.service;
 
 import com.Matheus.GestaoFinanceira.Transactions.entity.Expenses;
 import com.Matheus.GestaoFinanceira.Transactions.entity.Income;
-import com.Matheus.GestaoFinanceira.Transactions.entity.Transaction;
 import com.Matheus.GestaoFinanceira.Transactions.repository.ExpensesRepository;
 import com.Matheus.GestaoFinanceira.Transactions.repository.IncomesRepository;
-import com.Matheus.GestaoFinanceira.Transactions.repository.TransactionRepository;
 import com.Matheus.GestaoFinanceira.User.entity.User;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -23,7 +22,7 @@ public class DashboardService {
         this.incomesRepository = incomesRepository;
     }
 
-    //Atualizar e deixar que o banco cuide disso depois
+    //Deixar que banco de dados cuide dessa parte no futuro
     public BigDecimal currentBalance(User user) {
         List<Income> allIncome = incomesRepository.getAllIncomesByUserId(user.getId());
 
@@ -40,5 +39,68 @@ public class DashboardService {
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
 
         return totalIncomes.subtract(totalExpenses);
+    }
+
+    //Deixar que o banco de dados cuide dessa parte no futuro
+    public BigDecimal balanceExpenseForMonth(User user) {
+        List<Expenses> allExpense = expensesRepository.getAllExpensesByUserId(user.getId());
+
+        LocalDate now = LocalDate.now();
+        LocalDate firstDayOfMonth = now.withDayOfMonth(1);
+        LocalDate lastDayOfMonth = now.withDayOfMonth(now.lengthOfMonth());
+
+        return allExpense
+                .stream()
+                .filter(e -> e.getTransactionDate().isAfter(firstDayOfMonth))
+                .filter(e -> e.getTransactionDate().isBefore(lastDayOfMonth))
+                .map(Expenses::getValue)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    public BigDecimal balanceIncomeForMonth(User user) {
+        List<Income> allIncome = incomesRepository.getAllIncomesByUserId(user.getId());
+
+        LocalDate now = LocalDate.now();
+        LocalDate firstDayOfMonth = now.withDayOfMonth(1);
+        LocalDate lastDayOfMonth = now.withDayOfMonth(now.lengthOfMonth());
+
+        return allIncome
+                .stream()
+                .filter(i -> i.getTransactionDate().isBefore(lastDayOfMonth))
+                .filter(i -> i.getTransactionDate().isAfter(firstDayOfMonth))
+                .map(Income::getValue)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    public BigDecimal balanceExpenseForPreviousMonth(User user) {
+        List<Expenses> allExpenses = expensesRepository.getAllExpensesByUserId(user.getId());
+
+        LocalDate now = LocalDate.now();
+        LocalDate startPreviousMonth = now.minusMonths(1).withDayOfMonth(1);
+        LocalDate endPreviousMonth = now.minusMonths(1)
+                .withDayOfMonth(startPreviousMonth.lengthOfMonth());
+
+        return allExpenses
+                .stream()
+                .filter(e -> e.getTransactionDate().isBefore(endPreviousMonth))
+                .filter(e -> e.getTransactionDate().isAfter(startPreviousMonth))
+                .map(Expenses::getValue)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    public BigDecimal balanceIncomeForPreviousMonth(User user) {
+        List<Income> allIncomes = incomesRepository.getAllIncomesByUserId(user.getId());
+
+        LocalDate now = LocalDate.now();
+        LocalDate startPreviousMonth = now.minusMonths(1).withDayOfMonth(1);
+        LocalDate endPreviousMonth = now.minusMonths(1)
+                .withDayOfMonth(startPreviousMonth.lengthOfMonth());
+
+        return allIncomes
+                .stream()
+                .filter(i -> i.getTransactionDate().isBefore(endPreviousMonth))
+                .filter(i -> i.getTransactionDate().isAfter(startPreviousMonth))
+                .map(Income::getValue)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 }
