cec825b7258409ad2e3d209e943bb44d56b3ab87
MatheusCristophi <matheuscr909@gmail.com>
2026-07-01 06:59:09 -0300

    html e css da página de login finalizados

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LoginHeader } from '../../components/login-header/login-header';
 import { LoginFooter } from '../../components/login-footer/login-footer';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [LoginHeader, LoginFooter],
+  imports: [LoginHeader, LoginFooter, NgOptimizedImage],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
